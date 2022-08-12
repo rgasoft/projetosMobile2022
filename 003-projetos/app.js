79a89cc60e235fc759541656b1a830179e4facab
rgasoft <110498328+rgasoft@users.noreply.github.com>
2022-08-12 19:36:31 -0300

    Delete 003-projetos directory

--- 003-projetos/app.js
+++
@@ -1,7 +0,0 @@
-var salario = 2000.00;
-var aumento = 0.15;  
-var inflacao = 0.07;   
-var novosalario = salario + (aumento * salario);
-var novosalarioinflacao = novosalario - (inflacao * novosalario); 
-console.log (novosalario);
-console.log (novosalarioinflacao);  
