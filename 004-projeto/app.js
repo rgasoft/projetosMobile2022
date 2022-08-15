afda9a8ec6c47f9db59523fcb46f812c3d0a93f5
rgasoft <110498328+rgasoft@users.noreply.github.com>
2022-08-15 20:15:32 -0300

    Add files via upload

---
+++ 004-projeto/app.js
@@ -0,0 +1,19 @@
+//exercíco 1
+var num1 = 5;
+var num2 = 2;
+var num3 ; 
+console.log(num1);
+console.log(num2);
+console.log(num3);
+num3 = num1 / num2;
+console.log(num3);
+//exercíco 2
+num4 = num1%num2; // percentual recebe a sobra da divisão 
+console.log(num4);
+//exercíco 3
+num3_round = Math.round (num3);  // pacote de  calculos Math (tem vária opçoes). 
+console.log(num3_round);
+//exercíco 4
+var num5 = 12312214.19998789;
+num5 = num5.toFixed(2); // Define quantidade da casa decimal que fica entre parênteses.
+console.log(num5);  
