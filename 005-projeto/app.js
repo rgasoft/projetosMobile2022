295e2a45d86cd074b0ca1942f1b452960f9f240e
rgasoft <110498328+rgasoft@users.noreply.github.com>
2022-08-15 20:47:44 -0300

    Add files via upload

---
+++ 005-projeto/app.js
@@ -0,0 +1,6 @@
+var str01 = "1" + 2 + 2;  // aspas no primeiro dado transforma toda expressão em string .
+var str02 = 1 + 2 + 2;   // tratado com numérica
+var str03 = 1 + 2 + "2";  // aspas no final executa cálculo inicial e concatena com a string "2"
+console.log(str01);
+console.log(str02);
+console.log(str03);
