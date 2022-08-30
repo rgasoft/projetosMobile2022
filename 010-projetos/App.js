57125994b668a4cd6c0c7feb636ad3cfa4337824
rgasoft <110498328+rgasoft@users.noreply.github.com>
2022-08-29 21:16:23 -0300

    Add files via upload

--- 010-projetos/App.js
+++ 010-projetos/App.js
@@ -1,72 +1,68 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Button,Text, View , Alert, SafeAreaView } from 'react-native';
-import Constants from 'expo-Constants';
- 
- function Separator ()
-{ 
-  return <View style={styles.Separator} />
-}
+//import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Button, View, SafeAreaView, Text, Alert } from 'react-native';
+import Constants from 'expo-constants';
+
+const Separator = () => (
+  <View style={styles.separator} />
+);
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container} >
-<View>
-  <Button title="Aperte Aqui" onPress={() => {alert('Simples')}} />
-</View>[
-<Separator />
-</View>
-<Text style={style.title}>
-  Adjust the color in a way that looks standard on each platform. ON iOS, the color prop controls the color adjusts the text.
-   On Android , the color adjusts the background color of the button.
-  </Text>
-<Button title="Aperte aqui" color="#f194ff" onPress={() => { Alert('buton with adjusted color pressed')}} />
-<View>
-  <Separator/>
-</View>
-<Text style={styles.title >
-All Interaction  for the componets are desable.
-</Text>
-<Button title="Aperte aqui" disable onPress={() => {alert('Cannot press this one ')}} />
-</View>
-<separator/>
-</View>
-<Text style={styles.title}>
-  <Button
-  title="Botão esquerdo"
-  onPress={() => {alert('Left button pressed')}}
-  />
-  <Button
-  title="Botão direito"
-  onPress={() => {alert('Right button pressed')}}
- />
-</View>
-</View>
-<Separator />
-<Separator />
-<Separator />
-</SafeAreaView>
+   <SafeAreaView style={styles.container} >
+    <View>
+      <Text style={styles.title}>
+        O título e o manipulador onPress são obrigatórios. Recomenda-se definir acessibilidadeLabel para ajudar a tornar seu aplicativo utilizável por todos.      
+      </Text>
+      <Button        title="Aperte"        onPress={() => alert('Botão Simples')}
+      />
+    </View>
+    <Separator />
+    <View>
+      <Text style={styles.title}>
+      Ajuste a cor de uma maneira que pareça padrão em cada plataforma. No iOS, o suporte de cor controla a cor do texto. No Android, a cor ajusta a cor de fundo do botão.
+      </Text>
+      <Button
+        title="Aperte"        color="#f194ff"        onPress={() => alert('Botão com cor ajustada pressionado')}      />
+    </View>
+    <Separator />
+    <View>
+      <Text style={styles.title}>
+      Todas as interações para o componente estão desabilitadas.
+      </Text>
+      <Button        title="Aperte"       disabled        onPress={() => alert('Nao pode ser pressionado')}      />
+    </View>
+    <Separator />
+    <View>
+      <Text style={styles.title}>
+        Essa estratégia de layout permite que o título defina a largura do botão.
+      </Text>
+      <View style={styles.fixToText}>
+        <Button          title="Botão a esquerda"          onPress={() => alert('Botão a esquerda')}        />
+        <Button          title="Botão a direita"          onPress={() => alert('Botão a direita')}        />
+      </View>
+    </View>
+  </SafeAreaView>
   );
- }
- const styles = StyleSheet.create({
-  conatiner: {
+}
+
+const styles = StyleSheet.create({
+  container: {
     flex: 1,
-    marginTop: Constants.statusBarHeight,
+    //justifyContent: 'center',
     marginHorizontal: 16,
   },
-title: {
-  textAling: 'center',
-  marginVertical: 8,
-  }, 
-fixToText: {
-  FileSystemDirection: ' row',
-  JustifyContent: 'space-between'
-},
-separator: {
-  marginVertical: 8,
-  borderBottomColor: '#737373 ',
-  BorderBottomWidth: StyleSheet.hairlineWIdth,
-}, 
+  title: {
+    textAlign: 'center',
+    marginVertical: 8,
+  },
+  fixToText: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  separator: {
+    marginVertical: 8,
+    borderBottomColor: '#737373',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },  
 });
-
-  
-  
